fc96447b1253e295f325598d83b278837cfbf012
Leticia Costa Fernandes <leticia.fernandes@WP053604NRJBA.local>
2020-08-18 13:46:38 -0300

    refactoring to node

---
+++ exercises/modules/node/api.js
@@ -0,0 +1,19 @@
+let { users, posts } = require('./data');
+
+const getUserById = (id, cb) => {
+    // simulate API call
+    setTimeout(() => {
+      const user = users.find(user => user.id === id)
+      cb(user)
+    }, 150)
+}
+
+const getPostsForUser = (userId, cb) => {
+    // simulate API call
+    setTimeout(() => {
+      const res = posts.filter(post => post.createdBy === userId)
+      cb(res)
+    }, 150)
+}
+
+module.exports = { getUserById, getPostsForUser };
